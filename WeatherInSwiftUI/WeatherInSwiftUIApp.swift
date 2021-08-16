304e9b8ba8eac573554d9dc6d2035ec1f4d6e399
Marc <marcjacques@me.com>
2021-08-16 11:35:53 -0400

    add Background color
    add background color gradient

---
+++ WeatherInSwiftUI/WeatherInSwiftUIApp.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherInSwiftUIApp.swift
+//  WeatherInSwiftUI
+//
+//  Created by Marc Jacques on 8/16/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherInSwiftUIAPP: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
